2d7ceee5f5ffcca61735cdfac3abb206c99e3c65
Alexandru Mariuti <alex@mariuti.com>
2026-05-08 16:40:17 +0200

    fix

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,14 +1,4 @@
+import 'package:example/counter.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import 'test/ui/counter_display.dart';
-
-void main() => runApp(
-  MaterialApp(
-    home: Provider(
-      create: (_) => Counter(),
-      child: CounterDisplay(),
-      dispose: (context, provider) => provider.dispose(),
-    ),
-  ),
-);
+void main() => runApp(const MaterialApp(home: CounterPage()));
--- example/source/main.dart
+++ example/source/main.dart
@@ -1,13 +1,4 @@
+import 'package:example/counter.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import 'test/ui/counter_display.dart';
-
-void main() => runApp(
-  MaterialApp(
-    home: Provider(
-      create: (_) => Counter(),
-      child: CounterDisplay(),
-    ),
-  ),
-);
+void main() => runApp(const MaterialApp(home: CounterPage()));
